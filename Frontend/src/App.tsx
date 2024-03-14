a63af9f1e7a0dedb4ec26a2540e7d824fe4f7926
Ahmed Tarek <ahmedtarekabd2002@gmail.com>
2024-03-14 12:19:31 +0200

    Install & setup Material Tailwind.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './assets/css/Layout.css';
 import UserSettings from './Pages/UserSettings/UserSettings';
+import { Button } from '@material-tailwind/react';
 
 function App() {
   return (
     <div className='App'>
+      <Button size='sm'>Button</Button>
       <Router>
         <Routes>
           <Route path='/settings/:page' element={<UserSettings />} />
