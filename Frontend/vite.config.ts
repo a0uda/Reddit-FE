db629b16a07167485a41cabf0026beed8106da44
AKhaled22 <ahmedkhaled1029@gmail.com>
2024-03-19 22:02:26 +0200

    environment configuration

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import replace from '@rollup/plugin-replace';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    replace({
+      preventAssignment: true,
+      'process.env': JSON.stringify(process.env),
+    }),
+  ],
+});
