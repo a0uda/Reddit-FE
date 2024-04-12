57e0c7c76060451dfcd7c805d03dd78ff55e368d
a0uda <abdullah.oudaa@gmail.com>
2024-04-12 03:38:19 +0200

    Update User.tsx

--- Frontend/src/API/User.tsx
+++ Frontend/src/API/User.tsx
@@ -8,20 +8,29 @@ console.log('baseUrl ', baseUrl);
 const config = {
   headers: {
     'Content-Type': 'application/json',
-    token: localStorage.getItem('token'),
+    'Authorization': localStorage.getItem('token')
   },
   withCredentials: false,
 };
 const fetchRequest = async (endPoint: string) => {
   return await axios.get(baseUrl + endPoint, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('token')
+    },
     withCredentials: false,
-    headers: { 'Content-Type': 'application/json' },
   });
 };
 
 const patchRequest = async ({ newSettings, endPoint }) => {
   try {
-    const response = await axios.patch(baseUrl + endPoint, newSettings, config);
+    const response = await axios.patch(baseUrl + endPoint, newSettings, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('token')
+      },
+      withCredentials: false,
+    });
     console.log(response);
 
     return response.data;
@@ -34,7 +43,13 @@ const patchRequest = async ({ newSettings, endPoint }) => {
 
 const postRequest = async ({ endPoint, data }) => {
   try {
-    const response = await axios.post(baseUrl + endPoint, data, config);
+    const response = await axios.post(baseUrl + endPoint, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('token')
+      },
+      withCredentials: false,
+    });
     console.log(response, response.headers!.get('Authorization').slice(7));
 
     return {
