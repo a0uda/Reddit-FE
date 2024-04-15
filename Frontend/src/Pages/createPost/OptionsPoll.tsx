95dad5e27b33ebf0a72c69eee344f8c8f60279a0
ahmedwaleed01 <ahmed.waleed1303@gmail.com>
2024-04-16 01:39:36 +0200

    Add delete for poll options

--- Frontend/src/Pages/createPost/OptionsPoll.tsx
+++ Frontend/src/Pages/createPost/OptionsPoll.tsx
@@ -3,7 +3,7 @@ import RoundedButton from '../../Components/RoundedButton';
 import Input from '../../Components/Input';
 
 interface Option {
-  value: string;
+  option: string;
 }
 
 interface OptionsPollProps {
@@ -12,12 +12,18 @@ interface OptionsPollProps {
 
 function OptionsPoll({ setFieldValue }: OptionsPollProps) {
   const [options, setOptions] = useState<Option[]>([
-    { value: '' },
-    { value: '' },
+    { option: '' },
+    { option: '' },
   ]);
 
   const addOptionField = (): void => {
-    setOptions([...options, { value: '' }]);
+    setOptions([...options, { option: '' }]);
+  };
+
+  const removeOptionField = (index: number): void => {
+    const updatedOptions = options.filter((_, i) => i !== index);
+    setOptions(updatedOptions);
+    setFieldValue(`polls`, updatedOptions);
   };
 
   return (
@@ -49,17 +55,36 @@ function OptionsPoll({ setFieldValue }: OptionsPollProps) {
       </div>
 
       <div className='flex flex-col flex-grow space-y-4'>
-        {options.map((_option, index) => (
-          <Input
-            className='placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 !border !border-t-[rgb(176,190,197)] mt-4'
-            key={index}
-            placeholder={`option ${index + 1}`}
-            type='text'
-            NoCheck={true}
-            onChange={(e) =>
-              setFieldValue(`polls[${index}].option`, e.target.value)
-            }
-          />
+        {options.map((option, index) => (
+          <div key={index} className='flex items-center relative'>
+            <Input
+              className='flex-grow placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 !border !border-t-[rgb(176,190,197)] mt-4'
+              placeholder={`option ${index + 1}`}
+              type='text'
+              key={index}
+              NoCheck={true}
+              onChange={(e) =>
+                setFieldValue(`polls[${index}].option`, e.target.value)
+              }
+            />
+            {index >= 2 && (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6 absolute right-2 top-6'
+                onClick={() => removeOptionField(index)}
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0'
+                />
+              </svg>
+            )}
+          </div>
         ))}
 
         <div className='flex flex-row justify-between'>
