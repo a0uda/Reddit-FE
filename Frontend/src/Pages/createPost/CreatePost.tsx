ce7ab98960ef6bad6da9802f77c30b2fa049bd8c
AKhaled22 <ahmedkhaled1029@gmail.com>
2024-04-26 01:33:26 +0300

    Reapply "Merge pull request #35 from a0uda/schedulepost"
    This reverts commit 6fcbbd8387cf7d0b888d07b5df94100f59a0bb30.

--- Frontend/src/Pages/createPost/CreatePost.tsx
+++ Frontend/src/Pages/createPost/CreatePost.tsx
@@ -16,6 +16,7 @@ import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import OvalButton from './OvalButton';
 import SchedulePostComponent from './schedulePost';
+import * as yup from 'yup';
 
 // type FormSchema =
 //   | 'createPost'
@@ -65,14 +66,24 @@ const NewPost: React.FC = () => {
     oc_flag: false,
     spoiler_flag: false,
     nsfw_flag: false,
-    post_in_community_flag: false,
+    post_in_community_flag: true,
   });
 
   useEffect(() => {
-    const schema = Validation(formType);
-    setValidateSchema(schema);
+    const baseSchema = Validation(formType);
+    const schemaWithCommunityName = baseSchema.shape({
+      community_name: yup
+        .string()
+        .when('post_in_community_flag', (postInCommunityFlag, schema) => {
+          return postInCommunityFlag[0]
+            ? schema.required('Community name is required')
+            : schema.notRequired();
+        }),
+    });
+
+    setValidateSchema(schemaWithCommunityName);
     console.log(formType);
-  }, [formType]);
+  }, [formType, initialValues.post_in_community_flag]);
 
   const handleDivClick = (index: number) => {
     console.log(index);
