655ef104aafca4be9960e00f66c58cf2424ca2b6
ReemKhatab <113204827+ReemKhatab@users.noreply.github.com>
2024-04-25 18:19:13 +0200

    changes in comment file

--- Frontend/src/Pages/User/Comments.tsx
+++ Frontend/src/Pages/User/Comments.tsx
@@ -1,23 +1,45 @@
 import { fetchRequest } from '../../API/User';
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import LoadingProvider from '../UserSettings/Containers/LoadingProvider';
 import { CommentType } from '../../types/types';
 import Comment from '../../Components/Posts/Comment';
 import useSession from '../../hooks/auth/useSession';
+import { useEffect, useRef, useState } from 'react';
 
 function Comments() {
   const { user } = useSession();
-  const { data, error, isLoading } = useQuery(
-    ['userComments', 'comments'],
-    () => fetchRequest(`users/comments/${user?.username}`)
-  );
+  // const { data, error, isLoading } = useQuery(
+  //   ['userComments', 'comments'],
+  //   () => fetchRequest(`users/comments/${user?.username}`)
+  // );
+
+  const [response, setResponse] = useState<[CommentType]>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const fetchReq = useMutation(fetchRequest);
+  useEffect(() => {
+    if (user?.username) {
+      setIsLoading(true);
+      fetchReq.mutate(`users/comments/${user?.username}`, {
+        onSuccess: (data) => {
+          setIsLoading(false);
+          console.log('reem', data.data);
+          setResponse(data.data);
+        },
+        onError: (err) => {
+          setIsLoading(false); // Set loading state to false on error
+          setError(err); // Set error state
+        },
+      });
+    }
+  }, [user?.username]);
 
   return (
     <>
       <LoadingProvider error={error} isLoading={isLoading}>
-        {data && (
+        {response && (
           <>
-            {data.data.map((comment: CommentType) => (
+            {response.map((comment: CommentType) => (
               <Comment key={comment._id} comment={comment} />
             ))}
           </>
