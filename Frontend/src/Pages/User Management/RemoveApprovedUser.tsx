57dcb514a4ca19d7d04ef97db16e406c735f9017
Ahmed <81365347+ahmedwaleed01@users.noreply.github.com>
2024-04-30 20:17:06 +0300

    Finish remove Approved users

---
+++ Frontend/src/Pages/User Management/RemoveApprovedUser.tsx
@@ -0,0 +1,55 @@
+import { useParams } from 'react-router-dom';
+import { useMutation } from 'react-query';
+import { postRequest } from '../../API/User';
+import GeneralForm from './CommonForm';
+
+interface ApproveFormProps {
+  handleOpen: () => void;
+  open: boolean;
+  username: string;
+}
+interface valueDataType {
+  community_name: string;
+  username: string;
+}
+export default function RemoveApprovedUser(
+  props: ApproveFormProps
+): JSX.Element {
+  const { community_name } = useParams();
+
+  const initialValues: valueDataType = {
+    community_name: community_name ? community_name : '',
+    username: props.username,
+  };
+
+  const mutation = useMutation(postRequest, {
+    onSuccess: () => {
+      console.log('User removed successfully');
+    },
+    onError: () => {
+      console.log('Failed to Remove User');
+    },
+  });
+
+  const handleOnSubmit = (values: object) => {
+    console.log(values);
+    mutation.mutate({
+      endPoint: 'communities/unapprove-user',
+      data: values,
+    });
+  };
+
+  return (
+    <GeneralForm
+      HandleOnSubmitFunction={handleOnSubmit}
+      initialValues={initialValues}
+      content={`Are you sure you want to remove ${props.username} as an approved
+    user?`}
+      title='Confirm'
+      handleOpen={props.handleOpen}
+      open={props.open}
+      username={props.username}
+      buttonText='Remove'
+    />
+  );
+}
