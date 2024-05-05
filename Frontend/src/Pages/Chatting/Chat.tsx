9081a3850841864c1f9a200bf59b3fd6b02c014c
AKhaled22 <ahmedkhaled1029@gmail.com>
2024-05-05 15:17:50 +0300

    chatting

---
+++ Frontend/src/Pages/Chatting/Chat.tsx
@@ -0,0 +1,120 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Input from '../../Components/Input';
+import { Avatar } from '@material-tailwind/react';
+import { useMutation } from 'react-query';
+import { postRequest } from '../../API/User';
+
+interface Message {
+  id: number;
+  content: string;
+  author: string;
+}
+interface UserChatting {
+  _id: '66314d36c16d394bc516c17f';
+  otherUsername: 'Dayana.Buckridge';
+  lastMessageSender: 'Dayana.Buckridge';
+  lastMessageText: 'Conqueror cetera adsuesco arma communis vita.';
+  lastMessageTimestamp: '2024-04-30T19:57:35.732Z';
+}
+
+function Chat(props: { userChatting: UserChatting }) {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [msg, setMsg] = useState('');
+  const postReq = useMutation(postRequest);
+  const FetchMessages = async () => {
+    try {
+      const res = await axios.get(
+        `${process.env.VITE_BASE_URL}chats/${props.userChatting.otherUsername}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: localStorage.getItem('token'),
+          },
+        }
+      );
+      setMessages(res.data);
+      console.log(res.data, 'resss');
+    } catch (err) {
+      console.error('Error fetching data:', err);
+    }
+  };
+  useEffect(() => {
+    FetchMessages();
+  }, []);
+  function handlesend(e: any) {
+    e.preventDefault();
+    postReq.mutate(
+      {
+        endPoint: `chats/send/${props.userChatting.otherUsername}`,
+        data: {
+          message: msg,
+        },
+      },
+      {
+        onSuccess: () => {
+          alert('message sent');
+        },
+      }
+    );
+    console.log('Sssssssssssssssss', msg);
+  }
+  return (
+    <div className='grid grid-cols-1 xl:grid-cols-layout h-[500px]'>
+      <div className='hidden xl:block'></div>
+      <div className='container pt-4 flex flex-col h-full'>
+        <div className='border-b'> {props.userChatting.otherUsername}</div>
+
+        <div className='flex-grow overflow-y-auto'>
+          <div className='overflow-y-auto h-[70vh] mt-16'>
+            <div className='h-full'>
+              <div className='flex-grow flex flex-col justify-center items-center'>
+                <div>
+                  <Avatar
+                    variant='circular'
+                    alt='https://www.redditstatic.com/avatars/defaults/v2/avatar_default_1.png'
+                    src='https://www.redditstatic.com/avatars/defaults/v2/avatar_default_1.png'
+                    style={{ width: '40px', height: '40px' }}
+                    className='ms-2'
+                  />
+                </div>
+                <div>{props.userChatting.otherUsername}</div>
+              </div>
+              {messages.map((message, index) => (
+                <div key={index}>
+                  {index === 0 ||
+                  messages[index - 1].author !== message.author ? (
+                    <div className='font-bold'>
+                      <Avatar
+                        variant='circular'
+                        alt='https://www.redditstatic.com/avatars/defaults/v2/avatar_default_1.png'
+                        src='https://www.redditstatic.com/avatars/defaults/v2/avatar_default_1.png'
+                        style={{ width: '25px', height: '25px' }}
+                        className='ms-2'
+                      />
+                      {message.author}
+                    </div>
+                  ) : null}{' '}
+                  <div className='ms-10'>{message.content}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div>
+          <form onSubmit={handlesend}>
+            <Input
+              type='text'
+              placeholder='Message'
+              style={{ backgroundColor: '#DCDCDC' }}
+              NoCheck={true}
+              onChange={(e) => setMsg(e.target.value)}
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
--- Frontend/src/Pages/Chatting/ChatMain.tsx
+++ Frontend/src/Pages/Chatting/ChatMain.tsx
@@ -1,11 +1,36 @@
 import SideBar from './Sidebar';
 import CreateChat from './CreateChat';
-
+import { useSocketContext } from '../../Providers/SocketProvider';
+import { useEffect } from 'react';
+import Chat from './Chat';
+interface UserChatting {
+  _id: string;
+  otherUsername: string;
+  lastMessageSender: string;
+  lastMessageText: string;
+  lastMessageTimestamp: string;
+}
 const ChatMain = ({ page }: { page: 'create' | 'chat' }) => {
+  const { socket } = useSocketContext();
+  const ahmed: UserChatting = {
+    _id: '66314d36c16d394bc516c17f',
+    otherUsername: 'reem',
+    lastMessageSender: 'reem',
+    lastMessageText: 'Conqueror cetera adsuesco arma communis vita.',
+    lastMessageTimestamp: '2024-04-30T19:57:35.732Z',
+  };
+  useEffect(() => {
+    socket?.on('newMessage', (newMessage) => {
+      newMessage.shouldShake = true;
+      console.log(newMessage);
+    });
+
+    return () => socket?.off('newMessage');
+  }, [socket]);
   return (
     <div className='flex'>
       <SideBar />
-      {page == 'create' ? <CreateChat /> : <></>}
+      {page == 'create' ? <CreateChat /> : <Chat userChatting={ahmed} />}
     </div>
   );
 };
